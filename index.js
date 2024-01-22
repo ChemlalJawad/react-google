335d721e235cb09491fff2a3dd025c52f14d2c95
Chemlal, Jawad <jawad.chemlal@contraste.com>
2024-01-22 20:06:58 +0100

    edit port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 app.use(express.static('vite-project/dist'));
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
